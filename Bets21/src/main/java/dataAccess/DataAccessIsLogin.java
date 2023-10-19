16fbcee925ccf3474404b83caa3d76f136d708bc
anaig <anaig@Santiposa>
2023-10-19 19:05:15 +0200

    Gallegos proba aldaketa

---
+++ Bets21/src/main/java/dataAccess/DataAccessIsLogin.java
@@ -0,0 +1,254 @@
+package dataAccess;
+
+//hello
+//Aldaketa proba egiteko
+//Aldaketa sonarcloud
+//Aldaketa sonarcloud github-etik
+//Proba beste ordenagailu batetikk 
+//Beste proba bat
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import configuration.ConfigXML;
+import configuration.UtilDate;
+import domain.*;
+import exceptions.QuestionAlreadyExist;
+
+/**
+ * It implements the data access to the objectDb database
+ */
+public class DataAccessIsLogin {
+	protected static EntityManager db;
+	protected static EntityManagerFactory emf;
+
+	ConfigXML c = ConfigXML.getInstance();
+
+	public DataAccessIsLogin(boolean initializeMode) {
+
+		System.out.println("Creating DataAccess instance => isDatabaseLocal: " + c.isDatabaseLocal()
+				+ " getDatabBaseOpenMode: " + c.getDataBaseOpenMode());
+
+		open(initializeMode);
+
+	}
+
+	public DataAccessIsLogin() {
+		this(false);
+	}
+
+	/**
+	 * This is the data access method that initializes the database with some events
+	 * and questions. This method is invoked by the business logic (constructor of
+	 * BLFacadeImplementation) when the option "initialize" is declared in the tag
+	 * dataBaseOpenMode of resources/config.xml file
+	 */
+	public void initializeDB() {
+
+		db.getTransaction().begin();
+		try {
+
+			Calendar today = Calendar.getInstance();
+
+			int month = today.get(Calendar.MONTH);
+			month += 1;
+			int year = today.get(Calendar.YEAR);
+			if (month == 12) {
+				month = 0;
+				year += 1;
+			}
+
+			Event ev1 = new Event(1, "Atlético-Athletic", UtilDate.newDate(year, month, 17));
+			Event ev2 = new Event(2, "Eibar-Barcelona", UtilDate.newDate(year, month, 17));
+			Event ev3 = new Event(3, "Getafe-Celta", UtilDate.newDate(year, month, 17));
+			Event ev4 = new Event(4, "Alavés-Deportivo", UtilDate.newDate(year, month, 17));
+			Event ev5 = new Event(5, "Español-Villareal", UtilDate.newDate(year, month, 17));
+			Event ev6 = new Event(6, "Las Palmas-Sevilla", UtilDate.newDate(year, month, 17));
+			Event ev7 = new Event(7, "Malaga-Valencia", UtilDate.newDate(year, month, 17));
+			Event ev8 = new Event(8, "Girona-Leganés", UtilDate.newDate(year, month, 17));
+			Event ev9 = new Event(9, "Real Sociedad-Levante", UtilDate.newDate(year, month, 17));
+			Event ev10 = new Event(10, "Betis-Real Madrid", UtilDate.newDate(year, month, 17));
+
+			Event ev11 = new Event(11, "Atletico-Athletic", UtilDate.newDate(year, month, 1));
+			Event ev12 = new Event(12, "Eibar-Barcelona", UtilDate.newDate(year, month, 1));
+			Event ev13 = new Event(13, "Getafe-Celta", UtilDate.newDate(year, month, 1));
+			Event ev14 = new Event(14, "Alavés-Deportivo", UtilDate.newDate(year, month, 1));
+			Event ev15 = new Event(15, "Español-Villareal", UtilDate.newDate(year, month, 1));
+			Event ev16 = new Event(16, "Las Palmas-Sevilla", UtilDate.newDate(year, month, 1));
+
+			Event ev17 = new Event(17, "Málaga-Valencia", UtilDate.newDate(year, month + 1, 28));
+			Event ev18 = new Event(18, "Girona-Leganés", UtilDate.newDate(year, month + 1, 28));
+			Event ev19 = new Event(19, "Real Sociedad-Levante", UtilDate.newDate(year, month + 1, 28));
+			Event ev20 = new Event(20, "Betis-Real Madrid", UtilDate.newDate(year, month + 1, 28));
+
+			Question q1;
+			Question q2;
+			Question q3;
+			Question q4;
+			Question q5;
+			Question q6;
+
+			if (Locale.getDefault().equals(new Locale("es"))) {
+				q1 = ev1.addQuestion("¿Quién ganará el partido?", 1);
+				q2 = ev1.addQuestion("¿Quién meterá el primer gol?", 2);
+				q3 = ev11.addQuestion("¿Quién ganará el partido?", 1);
+				q4 = ev11.addQuestion("¿Cuántos goles se marcarán?", 2);
+				q5 = ev17.addQuestion("¿Quién ganará el partido?", 1);
+				q6 = ev17.addQuestion("¿Habrá goles en la primera parte?", 2);
+			} else if (Locale.getDefault().equals(new Locale("en"))) {
+				q1 = ev1.addQuestion("Who will win the match?", 1);
+				q2 = ev1.addQuestion("Who will score first?", 2);
+				q3 = ev11.addQuestion("Who will win the match?", 1);
+				q4 = ev11.addQuestion("How many goals will be scored in the match?", 2);
+				q5 = ev17.addQuestion("Who will win the match?", 1);
+				q6 = ev17.addQuestion("Will there be goals in the first half?", 2);
+			} else {
+				q1 = ev1.addQuestion("Zeinek irabaziko du partidua?", 1);
+				q2 = ev1.addQuestion("Zeinek sartuko du lehenengo gola?", 2);
+				q3 = ev11.addQuestion("Zeinek irabaziko du partidua?", 1);
+				q4 = ev11.addQuestion("Zenbat gol sartuko dira?", 2);
+				q5 = ev17.addQuestion("Zeinek irabaziko du partidua?", 1);
+				q6 = ev17.addQuestion("Golak sartuko dira lehenengo zatian?", 2);
+
+			}
+
+			LinkedList<Mugimendua> lista = new LinkedList<Mugimendua>();
+			Mugimendua mug = new Mugimendua(100, ev1, q1, "abibidea");
+			lista.add(mug);
+
+			User user1 = new User("user", "izena", "abizena", "user", 26, "asd@gmail.com", 0, lista);
+			//Admin user2 = new Admin("admin", "izena", "abizena", "admin", 26, "asd@gmail.com");
+			//Langile user3 = new Langile("langile", "izena", "abizena", "langile", 26, "asd@gmail.com");
+			Buzoia buzoi1 = new Buzoia(new LinkedList<Mezua>(), user1);
+			//Buzoia buzoi2 = new Buzoia(new LinkedList<Mezua>(), user2);
+			//Buzoia buzoi3 = new Buzoia(new LinkedList<Mezua>(), user3);
+			user1.setBuzoia(buzoi1);
+			//user2.setBuzoia(buzoi2);
+			//user3.setBuzoia(buzoi3);
+			db.persist(user1);
+			//db.persist(user2);
+			//db.persist(user3);
+
+			System.out.println(user1.printMugimenduak());
+
+			db.persist(q1);
+			db.persist(q2);
+			db.persist(q3);
+			db.persist(q4);
+			db.persist(q5);
+			db.persist(q6);
+
+			db.persist(ev1);
+			db.persist(ev2);
+			db.persist(ev3);
+			db.persist(ev4);
+			db.persist(ev5);
+			db.persist(ev6);
+			db.persist(ev7);
+			db.persist(ev8);
+			db.persist(ev9);
+			db.persist(ev10);
+			db.persist(ev11);
+			db.persist(ev12);
+			db.persist(ev13);
+			db.persist(ev14);
+			db.persist(ev15);
+			db.persist(ev16);
+			db.persist(ev17);
+			db.persist(ev18);
+			db.persist(ev19);
+			db.persist(ev20);
+
+			db.getTransaction().commit();
+			System.out.println("Db initialized");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Erabiltzaile isLogin(String log, String pass) {
+		if (log != null && pass != null) {
+			Erabiltzaile user = db.find(Erabiltzaile.class, log);
+			if (user == null)
+				return null;
+			if (!user.getPasahitza().equals(pass))
+				return null;
+			return user;
+		}
+		return null;
+	}
+
+	public boolean storeUser(User u) {
+		boolean ok = false;
+		try {
+			db.getTransaction().begin();
+			db.persist(u);
+			db.getTransaction().commit();
+			ok = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok;
+	}
+
+	public void open(boolean initializeMode) {
+
+		System.out.println("Opening DataAccess instance => isDatabaseLocal: " + c.isDatabaseLocal()
+				+ " getDatabBaseOpenMode: " + c.getDataBaseOpenMode());
+
+		String fileName = c.getDbFilename();
+		if (initializeMode) {
+			fileName = fileName + ";drop";
+			System.out.println("Deleting the DataBase");
+		}
+
+		if (c.isDatabaseLocal()) {
+			emf = Persistence.createEntityManagerFactory("objectdb:" + fileName);
+			db = emf.createEntityManager();
+		} else {
+			Map<String, String> properties = new HashMap<String, String>();
+			properties.put("javax.persistence.jdbc.user", c.getUser());
+			properties.put("javax.persistence.jdbc.password", c.getPassword());
+
+			emf = Persistence.createEntityManagerFactory(
+					"objectdb://" + c.getDatabaseNode() + ":" + c.getDatabasePort() + "/" + fileName, properties);
+
+			db = emf.createEntityManager();
+		}
+
+	}
+
+	public void close() {
+		db.close();
+		System.out.println("DataBase closed");
+	}
+	
+	public boolean removeUser(User u) {
+		boolean ok = false;
+		try {
+			User ev = db.find(User.class, u);
+			db.getTransaction().begin();
+			db.remove(ev);
+			db.getTransaction().commit();
+			ok = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok;
+	}
+
+}
+
+
+
---
+++ Bets21/src/test/java/IsLoginDABTest2.java
@@ -0,0 +1,88 @@
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import dataAccess.DataAccess;
+import dataAccess.DataAccessIsLogin;
+import domain.Erabiltzaile;
+import domain.Mugimendua;
+import domain.User;
+//import test.dataAccess.TestDataAccess;
+
+public class IsLoginDABTest2 {
+
+	static DataAccessIsLogin sut = new DataAccessIsLogin();
+	//static TestDataAccess TDA = new TestDataAccess();
+
+	private LinkedList<Mugimendua> lista = new LinkedList<Mugimendua>();
+	private User u = new User("josefina", "josefina", "pepeAb", "123", 20, "pepe@gmail.com", 0, lista);
+
+	@Before
+  	public void hasieratu() {
+		sut.open(false);
+		sut.storeUser(u);
+		sut.close();
+	}
+
+	@Test
+	public void test1() {
+		String log = null;
+		String pass = "123";
+		sut.open(false);
+		Erabiltzaile Emaitza = (Erabiltzaile) sut.isLogin(log, pass);
+		sut.close();
+		assertEquals(null, Emaitza);
+	}
+
+	@Test
+	public void test2() {
+		String log = "josefina";
+		String pass = null;
+		sut.open(false);
+		Erabiltzaile Emaitza = (Erabiltzaile) sut.isLogin(log, pass);
+		sut.close();
+		assertEquals(null, Emaitza);
+	}
+
+	@Test
+	public void test3() {
+		String log = "josef";
+		String pass = "123";
+		sut.open(false);
+		Erabiltzaile Emaitza = (Erabiltzaile) sut.isLogin(log, pass);
+		sut.close();
+		assertEquals(null, Emaitza);
+	}
+/*
+	@Test
+	public void test4() {
+		String log = "josefina";
+		String pass = "123";
+		System.out.println(u.toString());
+		sut.open(false);
+		Erabiltzaile Emaitza = (Erabiltzaile) sut.isLogin(log, pass);
+		sut.close();
+		System.out.println(Emaitza.toString());
+		assertEquals(u.getUserName(), Emaitza.getUserName());
+	}
+
+	@Test
+	public void test5() {
+		String log = "josefina";
+		String pass = "456";
+		sut.open(false);
+		Erabiltzaile Emaitza = (Erabiltzaile) sut.isLogin(log, pass);
+		assertEquals(null, Emaitza);
+		sut.close();
+	}
+*/
+	@After
+  	public void desegin() {
+		sut.open(false);
+		sut.removeUser(u);
+		sut.close();
+	}
+}
